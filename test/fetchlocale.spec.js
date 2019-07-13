915226b858021950a851ed6e1d6d97e111911ccb
Bram Verdonck <bram@exsconsult.com>
2019-07-13 19:44:06 +0200

    Testing: added all headers

--- test/fetchlocale.spec.js
+++ test/fetchlocale.spec.js
@@ -29,7 +29,9 @@ describe('lambda', () => {
         it('should return the default language if no valid accept-language is set and no language passed via query', function (cb) {
             return this.handler(this.event, this.context, (e, request) => {
                 expect(e).to.be.null
-                expect(request.headers['accept-language'][0].value).to.equal('en');
+                expect(request.headers['accept-language'][0].value).to.equal('en-US');
+                expect(request.headers['x-cvc-language'][0].value).to.equal('en');
+                expect(request.headers['x-cvc-locale'][0].value).to.equal('en-US');
                 return cb()
             })
         })
@@ -37,7 +39,9 @@ describe('lambda', () => {
             this.event.Records[0].cf.request.querystring = "fb_locale=nl-BE&something=empty"
             return this.handler(this.event, this.context, (e, request) => {
                 expect(e).to.be.null
-                expect(request.headers['accept-language'][0].value).to.equal('nl');
+                expect(request.headers['accept-language'][0].value).to.equal('nl-BE');
+                expect(request.headers['x-cvc-language'][0].value).to.equal('nl');
+                expect(request.headers['x-cvc-locale'][0].value).to.equal('nl-BE');
                 return cb()
             })
         })
@@ -45,7 +49,9 @@ describe('lambda', () => {
             this.event.Records[0].cf.request.querystring = "lang=nl"
             return this.handler(this.event, this.context, (e, request) => {
                 expect(e).to.be.null
-                expect(request.headers['accept-language'][0].value).to.equal('nl');
+                expect(request.headers['accept-language'][0].value).to.equal('nl-BE');
+                expect(request.headers['x-cvc-language'][0].value).to.equal('nl');
+                expect(request.headers['x-cvc-locale'][0].value).to.equal('nl-BE');
                 return cb()
             })
         })
@@ -53,7 +59,9 @@ describe('lambda', () => {
             this.event.Records[0].cf.request.headers['accept-language'].push({key: 'Accept-Language', value:'fr-CH, fr;q=0.9, en;q=0.8, de;q=0.7, *;q=0.5'})
             return this.handler(this.event, this.context, (e, request) => {
                 expect(e).to.be.null
-                expect(request.headers['accept-language'][0].value).to.equal('en');
+                expect(request.headers['accept-language'][0].value).to.equal('en-US');
+                expect(request.headers['x-cvc-language'][0].value).to.equal('en');
+                expect(request.headers['x-cvc-locale'][0].value).to.equal('en-US');
                 return cb()
             })
         })
@@ -61,7 +69,9 @@ describe('lambda', () => {
             this.event.Records[0].cf.request.headers['accept-language'].push({key: 'Accept-Language', value:'nl-BE,nl;q=0.9,*;q=0.8'})
             return this.handler(this.event, this.context, (e, request) => {
                 expect(e).to.be.null
-                expect(request.headers['accept-language'][0].value).to.equal('nl');
+                expect(request.headers['accept-language'][0].value).to.equal('nl-BE');
+                expect(request.headers['x-cvc-language'][0].value).to.equal('nl');
+                expect(request.headers['x-cvc-locale'][0].value).to.equal('nl-BE');
                 return cb()
             })
         })
@@ -69,7 +79,9 @@ describe('lambda', () => {
             this.event.Records[0].cf.request.headers['accept-language'].push({key: 'Accept-Language', value:'en-GB,en;q=0.9,*;q=0.8'})
             return this.handler(this.event, this.context, (e, request) => {
                 expect(e).to.be.null
-                expect(request.headers['accept-language'][0].value).to.equal('en');
+                expect(request.headers['accept-language'][0].value).to.equal('en-US');
+                expect(request.headers['x-cvc-language'][0].value).to.equal('en');
+                expect(request.headers['x-cvc-locale'][0].value).to.equal('en-US');
                 return cb()
             })
         })
@@ -78,7 +90,9 @@ describe('lambda', () => {
             this.event.Records[0].cf.request.headers['accept-language'].push({key: 'Accept-Language', value:'en-GB,en;q=0.9,*;q=0.8'})
             return this.handler(this.event, this.context, (e, request) => {
                 expect(e).to.be.null
-                expect(request.headers['accept-language'][0].value).to.equal('nl');
+                expect(request.headers['accept-language'][0].value).to.equal('nl-BE');
+                expect(request.headers['x-cvc-language'][0].value).to.equal('nl');
+                expect(request.headers['x-cvc-locale'][0].value).to.equal('nl-BE');
                 return cb()
             })
         })
@@ -86,7 +100,9 @@ describe('lambda', () => {
             this.event.Records[0].cf.request.headers['accept-language'].push({key: 'Accept-Language', value:'nl-NL,nl;q=0.9,en-US;q=0.8,en;q=0.7'})
             return this.handler(this.event, this.context, (e, request) => {
                 expect(e).to.be.null
-                expect(request.headers['accept-language'][0].value).to.equal('nl');
+                expect(request.headers['accept-language'][0].value).to.equal('nl-BE');
+                expect(request.headers['x-cvc-language'][0].value).to.equal('nl');
+                expect(request.headers['x-cvc-locale'][0].value).to.equal('nl-BE');
                 return cb()
             })
         })
